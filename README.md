5b4f210812c7d59f31bfb9a0b94759dad4ca3a22
glcz <gregoirelucazeau+github@gmail.com>
2026-07-28 22:27:19 +0200

    Ménage et orga

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Une collection de petits outils.
 
 ## Todo
 
-- [ ] Tirer au clair __init__ et __main__
+- [ ] Tirer au clair `__init__ ` et `__main__`
---
+++ toolkit/tools/mailfilters/gmailfilters.py
@@ -0,0 +1,12 @@
+# Objectif :
+# Récupérer une liste de domaines
+# y appender -from:
+# entourer la liste de (AND et )
+
+def exclusion(domaine):
+    print(f"-from:{domaine}"
+    )
+
+domaine = "google.com"
+
+exclusion()
