fc30f557a70ed7855074ff8bc84a64e956deb3ba
glcz <gregoirelucazeau+github@gmail.com>
2026-08-01 17:10:11 +0200

    chore: Code mort supprimé

--- toolkit/tools/cli.py
+++ toolkit/tools/cli.py
@@ -16,8 +16,6 @@ def toolkit():
     """Une collection de petits outils.""" # S'affiche dans --help
 
 for tool in tools:
-    #@toolkit.command(tool)
-    #def decouverte(tool_name=tool): # Important de passer la valeur de tool à tool_name sinon la boucle redéfinit tool à chaque passe
         module_path = tool + ".main" # On construit la chaine du module à importer (par exemple namegen.main)
         module = importlib.import_module(module_path) # Import du module
         toolkit.add_command(module.start, name=tool)
